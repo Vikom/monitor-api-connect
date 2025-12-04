a55db178f75e044d792881a43271cf043d1410b1
Viktor Miranda <info@viktormiranda.com>
2025-12-05 00:44:40 +0100

    Monitor server fix

---
+++ app/utils/monitor.server.js
@@ -0,0 +1,5 @@
+// app/utils/monitor.server.js
+// Server-only re-export of monitor functions
+// This file ensures monitor.js is never analyzed by the client bundler
+
+export * from './monitor.js';
